82c7cec76228d046fa42412a5ebd5372ecf064b9
Maxime <maxgo.maxoue@gmail.com>
2024-10-24 09:01:17 +0200

    fix: ex6 execution

--- ex6.sh
+++ ex6.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-#rm -rf stagiare/
+if [  -d "stagiare" ]; then
+	rm -rf stagiare/
+fi
 
-mkdir stagiare/
-cd stagiare/
+mkdir -p stagiare/photos/
 
-mkdir photos/
-cd photos
+# cd stagiare/
+
+# mkdir 
+# cd photos
 
 echo "Creation des fichiers en cours \n"
-typeset -i n_file=0
+touch stagiare/photos/{1..100}.jpg
+# typeset -i n_file=0
 
-while [ $n_file -lt 101 ]
-do
-	touch "$n_file.jpg"
-	n_file=n_file+1
-done
+# while [ $n_file -lt 101 ]
+# do
+# 	touch "$n_file.jpg"
+# 	n_file=n_file+1
+# done
 
-echo "Creation termine ! \n"
+# echo "Creation termine ! \n"
