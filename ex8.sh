f389ccf8fb45d490e089cb50835e59f717759273
Maxime <maxgo.maxoue@gmail.com>
2024-10-25 10:06:40 +0200

    feat: exo 8

--- ex8.sh
+++ ex8.sh
@@ -4,23 +4,18 @@ mot2=""
 mot3=""
 typeset -i num=0
 
-read -p 'Veuillez entrer 3 mots ' mot1 mot2 mot3
-read -p 'Veuillez entrer un nombre > 100 :' n
+while [ -z $mot1 ] || [ -z $mot2 ] || [ -z $mot3 ] || [ $num -eq 0 ]
+do
+	read -p 'Veuillez entrer 3 mots ' mot1 mot2 mot3
+	read -p 'Veuillez entrer un nombre > 100 :' num
+	if [ $num -le 100 ]
+	then
+		echo 'Rentrer un numero > 100'
+		num=0
+	fi
+done
 
-
-if [ -z $mot1 ] || [ -z $mot2 ] || [ -z $mot3 ] || [ $n -eq 0 ]
-then
-	echo 'Rentrer toutes les entrees'
-	exit 0
-fi
-
-if [ $n -lt 100 ]
-then
-	echo 'Rentrer un numero > 100'
-	exit 0 
-fi
-
-resuNum=$(($RANDOM%$n))
+resuNum=$(($RANDOM % $num))
 
 mot1=${mot1:0:2}
 mot2=${mot2:0:2}
@@ -31,11 +26,5 @@ then
 	echo 'Error dans le parsing des mots'
 	exit 1
 fi
-
-
 resu=$mot1$mot2$mot3$resuNum
-
-
-
 printf 'Le mot de passe genere est : '$resu
-
