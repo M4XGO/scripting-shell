a58369360ad4c79e9a6e26f978327e0140e9f5fe
Maxime <maxgo.maxoue@gmail.com>
2024-10-25 10:36:42 +0200

    feat(ex9): add permission control

--- ex9/ex9.sh
+++ ex9/ex9.sh
@@ -7,15 +7,23 @@
 # Version:        1.0
 # ===============================================
 
-varpath=""
+varpath="$1"
 
-read -p 'Saisir le chemin du répertoir a creer ' varpath
+#read -p 'Saisir le chemin du répertoir a creer ' varpath
 
-
-if [ -z $varpath ]
-then
-	echo "Remplissez le path"
-	exit 0
+#if [ -z $varpath ]
+#then
+#	echo "Remplissez le path"
+#	exit 0
+#fi
+if [ -z "$varpath" ]; then
+  echo 'Usage: $varpath as parameter'
+  exit 0
 fi
 
-mkdir -p $varpath
+if [ ! -w "$(dirname "$varpath")" ]; then
+  echo "Erreur: Vous n'avez pas les droits d'accès pour créer le répertoire à cet emplacement."
+  exit 0
+fi
+
+mkdir -p "$varpath"
